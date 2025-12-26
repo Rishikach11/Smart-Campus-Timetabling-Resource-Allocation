b6c9ec5f0fd116a4826144485f3d6c6ee2d2d7fe
Rishika <aachaudhary0360@gmail.com>
2025-12-26 11:25:25 +0530

    add backend folder structure and health api

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 
 const app = express();
-
 const PORT = 5000;
+
+app.use(express.json());
+
+const healthRoutes = require("./routes/health.routes");
+app.use("/api", healthRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
